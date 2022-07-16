d161f2eef05e081dc50ec7326e2ee510d95ccd79
okechukwu Aguagba <okeyaguagba@gmail.com>
2022-07-16 11:05:02 +0200

    deployment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adbc655d9a9ce4d69a359c01f29249ef-2131988574.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2b2423e66f9a4eb4bb62d3d9db92ea0-975244535.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
